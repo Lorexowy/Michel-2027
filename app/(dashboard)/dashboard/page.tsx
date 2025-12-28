dcebf6cb69518dc947aae2953472e11ebdc9d48e
Lorexowy <mateusz.michel7@gmail.com>
2025-12-28 22:55:23 +0100

    kolejna poprawka

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -300,7 +300,7 @@ export default function DashboardPage() {
                       cx="50%"
                       cy="50%"
                       labelLine={false}
-                      label={({ name, percent }) => `${name}: ${(percent * 100).toFixed(0)}%`}
+                      label={({ name, percent }) => `${name}: ${percent ? (percent * 100).toFixed(0) : 0}%`}
                       outerRadius={70}
                       fill="#8884d8"
                       dataKey="value"
